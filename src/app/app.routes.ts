6bbfcf8f1ed04b82a58f6cf8e4117532ea684e5a
Hiocra <163871322+Arioch-Ellunn@users.noreply.github.com>
2026-04-15 20:23:49 +0200

    Connexion component
    Page de connexion HTML/TS/CSS OK

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Connexion } from './connexion/connexion';
+import { Inscription } from './inscription/inscription';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'connexion', pathMatch: 'full' },
+  { path: 'connexion', component: Connexion },
+  { path: 'inscription', component: Inscription }
+];
