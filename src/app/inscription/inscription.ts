99a5d3f09f893196f6393f6bb71b31670cc6ff76
Hiocra <163871322+Arioch-Ellunn@users.noreply.github.com>
2026-04-16 16:11:07 +0200

    Page Inscription & review CSS

--- src/app/inscription/inscription.ts
+++ src/app/inscription/inscription.ts
@@ -1,9 +1,37 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-inscription',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, RouterLink],
   templateUrl: './inscription.html',
-  styleUrl: './inscription.css',
+  styleUrl: './inscription.css'
 })
-export class Inscription {}
+export class Inscription {
+  nom = '';
+  prenom = '';
+  email = '';
+  telephone = '';
+  motDePasse = '';
+  confirmMotDePasse = '';
+  showPassword = false;
+  showConfirm = false;
+  cgAcceptees = false;
+  erreur = '';
+
+  onSubmit() {
+    this.erreur = '';
+
+    if (this.motDePasse !== this.confirmMotDePasse) {
+      this.erreur = 'Les mots de passe ne correspondent pas.';
+      return;
+    }
+    if (!this.cgAcceptees) {
+      this.erreur = 'Veuillez accepter les conditions générales.';
+      return;
+    }
+    console.log('Inscription :', this.email);
+  }
+}
