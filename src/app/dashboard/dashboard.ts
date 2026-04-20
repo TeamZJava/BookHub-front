79d89898534609296925dc87f795d8af299d5d40
TeamZJava <githubjava@tutamail.com>
2026-04-20 16:30:56 +0200

    création différentes pages

---
+++ src/app/dashboard/dashboard.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [],
+  templateUrl: './dashboard.html',
+  styleUrl: './dashboard.css',
+  template: '<p>Page Dashboard — à venir</p>'
+})
+export class Dashboard {
+
+}
---
+++ src/app/profil/profil.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profil',
+  imports: [],
+  templateUrl: './profil.html',
+  styleUrl: './profil.css',
+  template: '<p>Page Profil — à venir</p>'
+})
+export class Profil {
+
+}
