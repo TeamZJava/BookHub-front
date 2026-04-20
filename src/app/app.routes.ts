f54aa4a878daa17268e764f1793d2ff1d1bfaceb
TeamZJava <githubjava@tutamail.com>
2026-04-20 16:33:51 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,21 @@
 import { Routes } from '@angular/router';
 import { Connexion } from './connexion/connexion';
 import { Inscription } from './inscription/inscription';
+import { Catalogue } from './catalogue/catalogue';
+import { BookDetail } from './book-detail/book-detail';
+import { Favoris } from './favoris/favoris';
+import { Dashboard } from './dashboard/dashboard';
+import { Profil } from './profil/profil';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'connexion', pathMatch: 'full' },
   { path: 'connexion', component: Connexion },
-  { path: 'inscription', component: Inscription }
+  { path: 'inscription', component: Inscription },
+  { path: 'catalogue', component: Catalogue },
+  { path: 'books/:id', component: BookDetail },
+  { path: 'favoris', component: Favoris },
+  { path: 'dashboard', component: Dashboard  },
+  { path: 'profil', component: Profil },
+  { path: '**', redirectTo: 'connexion' }
+];
 ];
