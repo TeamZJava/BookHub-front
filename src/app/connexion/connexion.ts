36ff7233e4981c6623e200d290b6892d0830bb7d
TeamZJava <githubjava@tutamail.com>
2026-04-23 15:13:27 +0200

    Update connexion.ts
    correction architecture app

--- src/app/connexion/connexion.ts
+++ src/app/connexion/connexion.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink, Router } from '@angular/router';
-import { AuthService } from '../services/auth';
+import { AuthService } from '../services/auth.service';
 import { LoginRequest } from '../models/login-request.model';
 
 @Component({
